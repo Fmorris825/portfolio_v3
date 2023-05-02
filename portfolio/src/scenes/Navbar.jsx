77f4388a6e6d42ea7de1f09dda3f62372fac92f1
Fred Morris <FredrickCPMorris@hotmail.com>
2023-05-02 02:36:12 -0500

    Added Mobile Menu and Mobile Menu Pop-up with Nav Links.

--- portfolio/src/scenes/Navbar.jsx
+++ portfolio/src/scenes/Navbar.jsx
@@ -6,9 +6,10 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   const lowerCasePage = page.toLowerCase();
   return (
     <AnchorLink
-      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
-        hover:text-yellow transition duration-500`}
-      href={`${lowerCasePage}`}
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      } hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
       onClick={() => setSelectedPage(lowerCasePage)}
     >
       {page}
@@ -18,22 +19,88 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
 
 const Navbar = ({ selectedPage, setSelectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
-  const isAboveSmallScreens = useMediaQuery("(mid-width: 768px)");
+  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
     <nav className={"z-40 w-full fixed top-0 py-6"}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">Code Fred</h4>
 
         {/*Desktop Nav */}
-        {isAboveSmallScreens ? (
-          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold"></div>
-        ) : (
-          <div>
+        {isDesktop ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            {" "}
             <Link
               page="Home"
               selectedPage={selectedPage}
               setSelectedPage={setSelectedPage}
             />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p-2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img alt="menu-icon" src="../assets/menu-icon.svg" />
+          </button>
+        )}
+
+        {/* MOBILE MENU POPUP */}
+        {!isDesktop && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+            {/* CLOSE ICON */}
+            <div className="flex justify-end p-12">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <img alt="close-icon" src="../assets/close-icon.svg" />
+              </button>
+            </div>
+            {/* MENU ITEMS */}
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
           </div>
         )}
       </div>
@@ -42,6 +109,3 @@ const Navbar = ({ selectedPage, setSelectedPage }) => {
 };
 
 export default Navbar;
-{
-  selectedPage, setSelectedPage;
-}
