e37796802e5634b675945ced69aa72e504325dd6
Fred Morris <FredrickCPMorris@hotmail.com>
2023-05-02 08:36:24 -0500

    Created animations for Landing component and added responsice styling.

--- portfolio/src/scenes/Landing.jsx
+++ portfolio/src/scenes/Landing.jsx
@@ -8,25 +8,67 @@ const Landing = ({ setSelectedPage }) => {
   return (
     <section className="md:flex md:justify-between md:items-center md:h-full gap 16 py-10">
       {/* Image Section */}
-      <div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+      <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
         {isAboveMediumScreens ? (
           <div
             className="relative z-0 ml-10 before:absolute before:-top-10 before:-left-10 before:[400px]
-          before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
+            before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]"
           >
             <img
               alt="profile"
-              className="hover:filter hover:saturate-150 trasition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+              className="hover:filter hover:saturate-150 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
               src="assets/portrait.png"
             />
           </div>
         ) : (
           <img
             alt="profile"
-            className="hover:filter hover:saturate-150 trasition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+            className="z-10 w-full max-w-[400px] md:max-w-[600px]"
             src="assets/portrait.png"
           />
         )}
+      </div>
+
+      {/* Main Section */}
+      <div className="z-30 basis-2/5 mt-12 md:mt-32">
+        {/* Headings */}
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <p className="text-6xl font-playfair z-10 text-center md:text-start">
+            Fredrick {""}
+            <span
+              className="xs:relative xs:text-deep-blue xs:font-semibold z-20 xs:before:content-brush
+              before:absolute before:-left-[25px] before:-top-[70px] before:z-[-1]"
+            >
+              Morris
+            </span>
+          </p>
+
+          <p className="mt-10 mb-7 text-sm text-center md:text-start">
+            Adipiscing arcu, in aliquam fringilla cursus. Elit arcu elementum
+            viverra malesuada sem ac faucibus dolor. Sagittis scelerisque.
+          </p>
+        </motion.div>
+        {/* Call to Actions */}
+        <motion.div
+          className="flex mt-5 justify-center md:justify-start"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.2, duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        ></motion.div>
       </div>
     </section>
   );
