7762c01639a7495e67358e15435c3e9bd33aa66e
Fred Morris <FredrickCPMorris@hotmail.com>
2023-05-03 21:11:13 -0500

    Tested submit form component. Works as expected.

--- portfolio/src/scenes/Contact.jsx
+++ portfolio/src/scenes/Contact.jsx
@@ -33,7 +33,7 @@ const Contact = () => {
       >
         <div>
           <p className="font-playfair font-semibold text-4xl">
-            <span className="text-yellow">CONTACT ME</span> TO GET STARTED
+            <span className="text-yellow">CONTACT ME</span>
           </p>
           <div className="flex md:justify-end my-5">
             <LineGradient width="w-1/2" />
@@ -71,7 +71,7 @@ const Contact = () => {
           <form
             target="_blank"
             onSubmit={onSubmit}
-            action="https://formsubmit.co/codefreddesign.com"
+            action="https://formsubmit.co/codefreddesign@outlook.com"
             method="POST"
           >
             <input
